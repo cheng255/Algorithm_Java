36f2c9b4afe6264bb5781819b0e916192dd29f21
cheng255 <18229094255@163.com>
2021-02-12 21:45:14 +0800

    环形单链表的约瑟夫问题  简单和进阶版本代码

--- linkedList/Josephus.java
+++ linkedList/Josephus.java
@@ -20,6 +20,42 @@ public class Josephus {
         System.out.println(josephus(node, 3).val);
 
     }
+
+    /**
+     * 进阶版本， 推出一个递归公式 old = (new + m + 1) % i + 1  old:Num(i)   new:Num(i-1)
+     *                                                      Num(i):表示长度为i的环中约瑟夫为题的解
+     *
+     *            详情看程序员代码面试指南
+     * @param node
+     * @param num
+     * @return
+     */
+    private static Node josephus1(Node node, int num) {
+        //1.找到循环链表的长度
+        int len = 1;
+        Node cur = node;
+        while (cur.next != node) {
+            len++;
+            cur = cur.next;
+        }
+        //2.直接递归
+        int res = getJos(len, num);//第res个结点就是答案
+        cur = node;
+        while (--res != 0) {
+            cur = cur.next;
+        }
+        //3.自成环
+        cur.next = cur;
+        return cur;
+    }
+
+    private static int getJos(int i, int m) {
+        if (i == 1) {
+            return 1;
+        }
+        return (getJos(i-1, m) + m + 1) % i + 1;
+    }
+
     private static Node josephus(Node node, int num) {
         int t = 0;
         Node cur = node;
